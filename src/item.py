9aa7962ffaf9770837b7894aa4c5deef758079fe
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-04-08 16:54:36 +0500

    Stretch

--- src/item.py
+++ src/item.py
@@ -1,4 +1,13 @@
 class Item(object):
     def __init__(self, name, description):
-        self.name
-        self.description
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return self.name
+
+    def on_take(self):
+        print(f"You have picked up {self.name}")
+
+    def on_drop(self):
+        print(f"You have dropped {self.name}")
--- src/room.py
+++ src/room.py
@@ -11,9 +11,3 @@ class Room(object):
         self.s_to = None
         self.w_to = None
         self.e_to = None
-
-    def add_item(self, item):
-        self.items += [item]
-
-    def remove_item(self, item_name):
-        self.items = [item in self.items if item_name != item.name]
