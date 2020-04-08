02b3f723abad4cf4210465b1561e7b27df0f4588
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-04-08 12:16:29 +0500

    Room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room(object):
+    def __init__(self, name, description, items=[]):
+        self.name = name
+        self.description = description
+        self.items = items
+        self.n_to = None
+        self.s_to = None
+        self.w_to = None
+        self.e_to = None
+
+    def add_item(self, item):
+        self.items += [item]
+
+    def remove_item(self, item_name):
+        self.items = [item in self.items if item_name != item.name]
